a2c2cc44ed0620d4fbbe729519013067d962ecb0
jflores0815 <flores.jaime0003@gmail.com>
2025-08-30 01:12:56 +0800

    UPDATE: viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/jflores-portfolio",
+  base: "/jflores-portfolio/",
   server: {
     port: 3000,
     open: true,
